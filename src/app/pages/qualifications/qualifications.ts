bd9e7e0fd052e40049b9fda896e2527f9b600e53
S_Safiuddeen (_SS_) <safideen.007@gmail.com>
2026-06-19 11:06:35 +0530

    Fix repository structure: separate backend and frontend folders

--- src/app/pages/qualifications/qualifications.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-qualifications',
-  imports: [],
-  templateUrl: './qualifications.html',
-  styleUrl: './qualifications.css',
-})
-export class Qualifications {}
