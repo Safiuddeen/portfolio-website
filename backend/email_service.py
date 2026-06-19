aba38bba5d9ad21bdfe0ed35e616fa7424b0d4bd
S_Safiuddeen (_SS_) <safideen.007@gmail.com>
2026-06-19 19:33:14 +0530

    chage the SMTP_PORT

--- backend/email_service.py
+++ backend/email_service.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT",587 ))
+SMTP_PORT = int(os.getenv("SMTP_PORT",465 ))
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
 RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
